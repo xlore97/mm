dbabb368bf609360a8e34f2b5a2ae1925dcc7990
Xlore97 <lorenzo.manetti.photo@gmail.com>
2025-11-17 19:20:54 +0100

    Fix merge

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -2,5 +2,6 @@ import SingleProduct from "./SingleProduct";
 
 export default function ProductsList() {
   // TODO: riempire questo componente nel branch specifico
+  // Puoi usare il componente SingleProduct per ogni prodotto nella lista
   return <SingleProduct />;
 }
--- src/components/SingleProducts.jsx
+++
@@ -1,5 +0,0 @@
-export default function SingleProduct() {
-  // TODO: riempire questo componente nel branch specifico
-  // Ciao Lorenzo, qui va il codice del componente SingleProduct
-  return <></>;
-}
