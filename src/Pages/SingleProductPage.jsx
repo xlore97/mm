c8ffb511185ccc2a72b19193bbabe38b3ffa9bf5
Sara Salzano <sarasalzano95@gmail.com>
2025-11-17 17:07:05 +0100

    aggiunta component bigsingleproduct

--- src/Pages/SingleProductPage.jsx
+++ src/Pages/SingleProductPage.jsx
@@ -1,6 +1,12 @@
 import SuggestedProducts from "../components/SuggestedProducts";
+import BigSingleProduct from "../components/BigSingleProduct";
 
 export default function SingleProductPage() {
   // TODO: riempire questo componente nel branch specifico
-  return <SuggestedProducts />;
+  return (
+    <>
+      <BigSingleProduct />
+      <SuggestedProducts />
+    </>
+  );
 }
