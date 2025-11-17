f80fdd6c8ed99faba5c83782c5b84bb0ba39a02e
Sara Salzano <sarasalzano95@gmail.com>
2025-11-17 14:06:21 +0100

    inserisci productlist nel component

--- src/components/Bestsellers.jsx
+++ src/components/Bestsellers.jsx
@@ -1,4 +1,6 @@
+import ProductList from "./ProductsList";
+
 export default function Bestsellers() {
   // TODO: riempire questo componente nel branch specifico
-  return <></>;
+  return <ProductList />;
 }
