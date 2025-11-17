028634a5183cb3cc36c57327e1ea175f1857180c
Sara Salzano <sarasalzano95@gmail.com>
2025-11-17 10:11:00 +0100

    import bestsellers

--- src/components/LatestArrivals.jsx
+++ src/components/LatestArrivals.jsx
@@ -1,14 +1,4 @@
-import SingleProduct from "./SingleProduct";
-
 export default function LatestArrivals() {
   // TODO: riempire questo componente nel branch specifico
-  return (
-    <div className="latest-arrivals-container">
-    <SingleProduct />
-    <SingleProduct />
-    <SingleProduct />
-    <SingleProduct />
-    <SingleProduct />
-    </div>
-  );
+  return <></>;
 }
