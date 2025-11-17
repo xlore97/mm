c737f8523f1d8752c66673f0f9d0cdda1e416dd0
Xlore97 <lorenzo.manetti.photo@gmail.com>
2025-11-17 19:11:39 +0100

    Final stage

--- src/components/SingleProducts.jsx
+++ src/components/SingleProducts.jsx
@@ -1,4 +1,5 @@
 export default function SingleProduct() {
   // TODO: riempire questo componente nel branch specifico
+  // Ciao Lorenzo, qui va il codice del componente SingleProduct
   return <></>;
 }
