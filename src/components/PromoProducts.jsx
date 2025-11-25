4c5679a68d19b8368551c4a2c45598fd2d779c1d
riccardo-ottavi <ottavi.ps@libero.it>
2025-11-25 15:36:09 +0100

    inserisci i prodotti in promo nella sezione della home

--- src/components/PromoProducts.jsx
+++ src/components/PromoProducts.jsx
@@ -8,8 +8,22 @@ export default function PromoProducts() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/promotions")
-      .then((res) => setProducts(res.data.data))
+      .get("http://localhost:3000/api/products") // prendi tutti i prodotti
+      .then((res) => {
+        const allProducts = res.data.data || [];
+
+        // Filtra solo prodotti con promo attiva (> 0) e calcola special_price
+        const promoProducts = allProducts
+          .filter((p) => Number(p.promo) > 0)
+          .map((p) => ({
+            ...p,
+            special_price: Number(
+              (p.regular_price * (1 - Number(p.promo) / 100)).toFixed(2)
+            ),
+          }));
+
+        setProducts(promoProducts);
+      })
       .catch((err) => console.error(err));
   }, []);
 
